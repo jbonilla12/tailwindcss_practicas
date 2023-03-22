b8ac96aa42caf5e945dc82c8ac83134382e6e231
jbonilla12 <joshua.lino12@gmail.com>
2023-03-22 12:42:06 -0600

    proyecto_2

--- Proyecto/tailwind.config.js
+++ Proyecto/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/estilos.css"],
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      rale: ['Raleway', 'sans-serif']
+    },
+    extend: {
+      colors:{
+        danger:'#FC2947',
+        info:{
+          100: '#FFDEB9',
+          200: '#7149C6'
+        }
+      }
+    },
   },
   plugins: [],
 }
