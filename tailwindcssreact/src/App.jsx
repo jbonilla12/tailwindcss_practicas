4fd67fe70107246b02f019a396eaf64688959c24
jbonilla12 <joshua.lino12@gmail.com>
2023-03-27 15:32:02 -0600

    react-tailwind_3

--- tailwindcssreact/src/App.jsx
+++ tailwindcssreact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import React from "react";
 
 const App =()=> {
   return (
-    <h1>Hola Tailwindcss</h1>
+    <div>
+      <h1>Hola Tailwindcss</h1>
+      <h1>Hola Tailwindcss</h1>
+      <button className="py-2 px-4 rounded bg-blue-500 text-white">Boton</button>
+    </div>
     
   );
 }
